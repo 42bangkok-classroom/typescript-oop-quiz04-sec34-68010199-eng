9b8410ddedea43846da7d75de6ae7fc4f02f0938
unknown <68010199@kmitl.ac.th>
2026-03-02 19:40:06 +0700

    Updates

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,44 +1,36 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Delete,
-  Param,
-  Query,
-  Body,
-} from "@nestjs/common";
-import { MissionService } from "./mission.service";
-import type { Mission } from "./mission.interface";
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { MissionService } from './mission.service';
+import type { Mission } from './mission.interface';
 
-@Controller("missions")
+@Controller('missions')
 export class MissionController {
   constructor(private readonly missionService: MissionService) {}
-
-  @Get("summary")
-  getSummary() {
-    return this.missionService.getSummary();
-  }
 
   @Get()
   findAll(): Mission[] {
     return this.missionService.findAll();
   }
 
-  @Get(":id")
-  findOne(
-    @Param("id") id: string,
-    @Query("clearance") clearance?: string
-  ): Mission {
-    return this.missionService.findOne(id, clearance);
+  @Get(':id')
+  findOne(@Param('id') id: string): Mission {
+    return this.missionService.findOne(id);
   }
 
   @Post()
-  create(@Body() body: any): Mission {
+  create(
+    @Body()
+    body: {
+      codename: string;
+      riskLevel: string;
+      targetName: string;
+      startDate: string;
+    },
+  ): Mission {
     return this.missionService.create(body);
   }
 
-  @Delete(":id")
-  remove(@Param("id") id: string) {
+  @Delete(':id')
+  remove(@Param('id') id: string): { message: string } {
     return this.missionService.remove(id);
   }
 }
